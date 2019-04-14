cf723864ca13d87d434284716d73d1cd196ec9ee
SamSchott <ss2151@cam.ac.uk>
2019-04-14 12:54:31 +0100

    Update example.m

--- examples/example.m
+++ examples/example.m
@@ -1,23 +1,17 @@
 %% standard FET parameters
 epsilon = 2.05; % dielectric constant CYTOP
+epsilon_0 = 8.854187817*1e-12; % in F/m
+
+pars.C = epsilon_0*epsilon/pars.d; % dielectric capacitance in F/m^2
 pars.W = 1e-3; % channel width in m
 pars.L = 20*1e-6; % channel length in m
 pars.d = 620*1e-9; % dielectric thickness in m
 
-
-%% FI-ESR parameters
-% epsilon = 3.6; % dielectric constant PMMA
-% pars.W = 0.243; % channel width in m
-% pars.L = 100*1e-6; % channel length in m
-% pars.d = 500*1e-9; % dielectric thickness in m
-
-%% Fit interval for mobility
-pars.Vfit = 20; % voltage interval for the monility fit in V
-
 %% calculate capacitance per unit area for the device architecture
-epsilon_0 = 8.854187817*1e-12; % in F/m
-pars.C = epsilon_0*epsilon/pars.d; % dielectric capacitance in F/m^2
 
-%% mobility calculation
-[mobSat, mobLin] = MobilityCalc(data, pars);
+%% read and plot transfer curve data
+data = FETDataRead();
+TransferDataPlot(data);
+
+%% calculate mobility
 [vg, mobSatVg, mobLinVg] = MobilityVsVg(data, pars);
